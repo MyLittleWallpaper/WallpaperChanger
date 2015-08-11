bba5d2a2b2ac42c9c9f5084740779be13c5d9e0f
Petri Haikonen <petri.haikonen@ecxol.net>
2015-08-11 08:17:35 +0300

    PEP 8

--- wallpaper_change.py
+++ wallpaper_change.py
@@ -13,16 +13,23 @@ except ImportError:
     from urllib.request import urlopen
 
 try:
-	# Python 2
+    # Python 2
     from urllib import quote_plus
 except ImportError:
-	# Python 3
+    # Python 3
     from urllib.parse import quote_plus
 
-import os, sys, getopt, json, random, getopt, subprocess
+import os
+import sys
+import getopt
+import json
+import random
+import getopt
+import subprocess
 
-# Default settings, please create settings.ini file instead of editing this file
-wallpaperGetUrls = ["http://www.mylittlewallpaper.com/c/all/api/v2/random.json?size=2&limit=1"]
+# Default settings, create settings.ini file instead of editing this file
+wallpaperGetUrls =
+["http://www.mylittlewallpaper.com/c/all/api/v2/random.json?size=2&limit=1"]
 desktopEnvironment = "gnome"
 wallpaperSaveFolder = "wallpapers"
 favouritesUsername = ""
@@ -42,19 +49,29 @@ if os.path.exists("settings.ini"):
     config.read("settings.ini")
     if config.has_option("MyLittleWallpaperChanger", "wallpaperGetUrls"):
         urlsString = config.get("MyLittleWallpaperChanger", "wallpaperGetUrls")
-        wallpaperGetUrls = urlsString.split(" ");
-    desktopEnvironment = config.get("MyLittleWallpaperChanger", "desktopEnvironment")
-    wallpaperSaveFolder = config.get("MyLittleWallpaperChanger", "wallpaperSaveFolder")
+        wallpaperGetUrls = urlsString.split(" ")
+    desktopEnvironment = config.get("MyLittleWallpaperChanger",
+                                    "desktopEnvironment")
+    wallpaperSaveFolder = config.get("MyLittleWallpaperChanger",
+                                     "wallpaperSaveFolder")
     if config.has_option("MyLittleWallpaperChanger", "favouritesUsername"):
-        favouritesUsername = config.get("MyLittleWallpaperChanger", "favouritesUsername")
+        favouritesUsername = config.get("MyLittleWallpaperChanger",
+                                        "favouritesUsername")
     if config.has_option("MyLittleWallpaperChanger", "favouritesToken"):
-        favouritesToken = config.get("MyLittleWallpaperChanger", "favouritesToken")
+        favouritesToken = config.get("MyLittleWallpaperChanger",
+                                     "favouritesToken")
 
 if favouritesUsername and favouritesToken:
-    import hashlib, uuid
+    import hashlib
+    import uuid
     requestId = uuid.uuid4().hex
-    urlHash = hashlib.sha256(str(favouritesUsername + favouritesToken + requestId).encode('utf-8')).hexdigest()
-    wallpaperGetUrls = ["http://www.mylittlewallpaper.com/c/all/api/v2/favourites.json?limit=1&sort=random&requestId=" + requestId + "&userName=" + quote_plus(favouritesUsername) + "&hash=" + urlHash]
+    urlHash = hashlib.sha256(str(favouritesUsername + favouritesToken +
+                             requestId).encode('utf-8')).hexdigest()
+    wallpaperGetUrls = ["http://www.mylittlewallpaper.com/c/all/api/v2/" +
+                        "favourites.json?limit=1&sort=random&requestId=" +
+                        requestId + "&userName=" +
+                        quote_plus(favouritesUsername) + "&hash=" + urlHash]
+
 
 def get_wallpaper():
     if not os.path.exists(wallpaperSaveFolder):
@@ -65,7 +82,8 @@ def get_wallpaper():
 
     # Fetch json from server
     try:
-        jsonData = json.loads(urlopen(wallpaperGetUrl, timeout = 60).read().decode('utf-8'))
+        jsonData = json.loads(urlopen(wallpaperGetUrl, timeout=60).read().
+                              decode('utf-8'))
     except Exception as e:
         print(e)
         return ""
@@ -76,9 +94,10 @@ def get_wallpaper():
         imageName = os.path.basename(fullImageUrl)
         if not os.path.exists(os.path.join(wallpaperSaveFolder, imageName)):
             try:
-                imageData = urlopen(fullImageUrl, timeout = 60).read()
+                imageData = urlopen(fullImageUrl, timeout=60).read()
                 if len(imageData) > 0:
-                    fileHandler = open(os.path.join(wallpaperSaveFolder, imageName), "wb")
+                    fileHandler = open(os.path.join(wallpaperSaveFolder,
+                                                    imageName), "wb")
                     fileHandler.write(imageData)
                     fileHandler.close()
                 else:
@@ -90,18 +109,22 @@ def get_wallpaper():
         return imageName
     return ""
 
+
 def change_wallpaper(wallpaper_uri):
     # Todo: Add support for other desktop environments
     try:
         if desktopEnvironment == "gnome":
-            os.system("gsettings set org.gnome.desktop.background picture-uri '%s'" % (wallpaper_uri))
+            os.system("gsettings set org.gnome.desktop.background " +
+                      "picture-uri '%s'" % (wallpaper_uri))
         elif desktopEnvironment == "fluxbox":
             try:
                 subprocess.Popen(["fbsetbg", wallpaper_uri])
             except:
-                sys.stderr.write("Failed to set desktop wallpaper. Please install fbsetbg.")
+                sys.stderr.write("Failed to set desktop wallpaper. Please " +
+                                 "install fbsetbg.")
         else:
-            sys.stderr.write("Failed to set desktop wallpaper. Unsupported desktop environment.")
+            sys.stderr.write("Failed to set desktop wallpaper. Unsupported " +
+                             "desktop environment.")
             return False
         return True
     except Exception as e:
@@ -110,5 +133,6 @@ def change_wallpaper(wallpaper_uri):
 
 wallpaperFilename = get_wallpaper()
 if wallpaperFilename:
-    file_path = os.path.abspath(os.path.join(wallpaperSaveFolder, wallpaperFilename))
+    file_path = os.path.abspath(os.path.join(wallpaperSaveFolder,
+                                             wallpaperFilename))
     change_wallpaper("file://" + file_path)
